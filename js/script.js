203d76ba673aa8543a5e968a905c97a4bae6e7aa
AkininVladimir <akinin_vv@mail.ru>
2022-01-29 20:13:42 +0300

    Updated file JS

--- js/script.js
+++ js/script.js
@@ -6,9 +6,11 @@
 let menu = document.getElementsByClassName('menu') [0] ,
     menuItem = document.getElementsByClassName('menu-item'),
     tit = document.getElementById ("title"),
-    adv = document.getElementsByClassName('adv') [0];
+    adv = document.getElementsByClassName('adv') [0],
+    prom = document.getElementById ('prompt');
 
 menu.insertBefore(menuItem[2], menuItem[1]);
 document.body.style.background = "url('img/apple_true.jpg')";
 title.textContent = 'Мы продаем только подлинную технику Apple';
 adv.remove();
+prom.textContent = 'Как Вы относитесь к продукции Apple?';
